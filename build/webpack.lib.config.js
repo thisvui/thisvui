49fbf2fed8584ce938840429d3384f485ad6ca4f
ncanatta <ncanatta@gmail.com>
2019-03-17 15:53:52 -0300

    Adds better cross browser support
    1) A.browserslistrc file was added to improve support for:
    * Firefox
    * Chrome
    * Safari >= 10
    * Opera
    * Edge
    * IE 11
    
    2) Little code cleanup in webpack config.
    3) Improved babel config.

--- build/webpack.lib.config.js
+++ build/webpack.lib.config.js
@@ -1,73 +1,56 @@
 /*** js bundling ***/
-const path = require('path');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const TerserPlugin = require('terser-webpack-plugin');
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = env => {
   return {
-    entry: './src/main.js',
+    entry: "./src/main.js",
     output: {
-      library: 'thisvui',
-      libraryTarget: 'umd',
-      path: path.resolve(__dirname, '../dist'),
-      filename: env && env.production ? 'thisvui.min.js': 'thisvui.js'
+      library: "thisvui",
+      libraryTarget: "umd",
+      path: path.resolve(__dirname, "../dist"),
+      filename: env && env.production ? "thisvui.min.js" : "thisvui.js"
     },
-    plugins: [
-      new VueLoaderPlugin()
-    ],
+    plugins: [new VueLoaderPlugin()],
     resolve: {
       extensions: [".js", ".vue", ".css"]
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     devServer: {
       port: 3001
     },
     optimization: {
       minimize: true,
-      minimizer: [new TerserPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true,
-        terserOptions: {
-          ecma: 6,
-          mangle: true
-        }
-      })]
+      minimizer: [
+        new TerserPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true,
+          terserOptions: {
+            ecma: 6,
+            mangle: true
+          }
+        })
+      ]
     },
     module: {
       rules: [
         {
           test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/,
-          options: {
-            presets: ['@babel/preset-env'],
-            "plugins": [
-              [
-                "@babel/transform-runtime",
-                {
-                  "corejs": false,
-                  "helpers": false,
-                  "regenerator": true,
-                  "useESModules": false
-                }
-              ]
-            ]
-          }
+          loader: "babel-loader",
+          exclude: /node_modules/
         },
         {
           test: /\.vue$/,
-          use: 'vue-loader'
+          use: "vue-loader"
         },
         {
           test: /\.css$/,
-          use: [
-            'vue-style-loader',
-            'css-loader'
-          ],
+          use: ["vue-style-loader", "css-loader"]
         }
       ]
     },
-    mode: env && env.production ? 'production' : 'development'
-  }
+    mode: env && env.production ? "production" : "development"
+  };
 };
