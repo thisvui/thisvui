97bf2b3235378e4c7b6e7606f8b09e7e5c3242d3
ncanatta <ncanatta@gmail.com>
2019-07-05 13:40:31 -0400

    Implements render function in TFile component
    1) A render function was implemented to render the TFile component
    more efficiently and reduce bundle size.
    
    2) Fixed bug when hasPreview prop is 'false'.

--- src/components/TFile/TFile.js
+++ src/components/TFile/TFile.js
@@ -1,73 +1,6 @@
-<template>
-  <div :class="getContainerClass">
-    <div :class="getClasses">
-      <label class="file-label file-label-container">
-        <input
-          :id="id"
-          class="file-input"
-          type="file"
-          :disabled="isUploading"
-          ref="files"
-          @change="handleFilesUpload()"
-          :accept="accept"
-        />
-        <span class="file-cta">
-          <span class="file-icon">
-            <t-icon
-              :preserve-defaults="!overrideDefaults"
-              :icon="$thisvui.icons.upload"
-            ></t-icon>
-          </span>
-          <span class="file-label">
-            <p v-if="isInitialUpload">
-              {{ label }}
-            </p>
-            <p v-if="isUploading">Uploading {{ fileCount }} files...</p>
-          </span>
-        </span>
-        <span class="file-name" v-if="fileHasName"> {{ fileName }}</span>
-      </label>
-      <t-buttons has-addons v-if="hasFiles" :class="getButtonContainerClasses">
-        <span @click="reset()" v-if="!isUploading" :class="getClearClasses">
-          <t-icon :icon="clearIcon"></t-icon
-          ><span v-if="!hideButtonsLabels">{{ clearText }}</span>
-        </span>
-        <span
-          @click="submitFiles()"
-          v-if="showSubmitButton"
-          :class="getSubmitClasses"
-        >
-          <t-icon :icon="submitIcon"></t-icon
-          ><span v-if="!hideButtonsLabels">{{ submitText }}</span>
-        </span>
-      </t-buttons>
-    </div>
-    <div :class="getPreviewClasses" v-if="showPreview && hasPreview">
-      <h2 v-if="isUploadSuccess" class="is-size-6">
-        Uploaded {{ files.length }} file(s) successfully.
-      </h2>
-      <t-thumbnails>
-        <t-thumbnail v-for="(file, key) in files" :key="key">
-          <img class="image is-128x128" v-bind:ref="`image${parseInt(key)}`" />
-          <h1>{{ file.name }}</h1>
-        </t-thumbnail>
-      </t-thumbnails>
-    </div>
-    <t-box v-if="isUploadFailed">
-      <h2>Uploaded failed.</h2>
-      <p>
-        <a href="javascript:void(0)" @click="reset()">Try again</a>
-      </p>
-      <pre>{{ uploadError }}</pre>
-    </t-box>
-  </div>
-</template>
-
-<script>
 import input from "../../mixins/input";
 import alignment from "../../mixins/alignment";
 import icons from "../../mixins/icons";
-import CssArchitect from "../../utils/css-architect";
 import TBox from "../TBox/TBox";
 import TIcon from "../TIcon/TIcon";
 import TThumbnails from "../TThumbnail/TThumbnails";
@@ -75,6 +8,9 @@ import TThumbnail from "../TThumbnail/TThumbnail";
 import TButtons from "../TButton/TButtons";
 import TButton from "../TButton/TButton";
 import TTags from "../TTag/TTags";
+
+import CssArchitect from "../../utils/css-architect";
+import ElementArchitect from "../../utils/element-architect";
 
 const STATUS_INITIAL = 0;
 const STATUS_SAVING = 1;
@@ -168,7 +104,7 @@ export default {
       currentStatus: null,
       showPreview: false,
       fileName: null,
-      fileName: 0
+      fileCount: 0
     };
   },
   computed: {
@@ -293,8 +229,10 @@ export default {
       this.fileName = uploadedFiles[0].name;
       this.fileCount = this.files.length;
       // Generate image previews for the uploaded files
-      this.showPreview = true;
-      this.getImagePreviews();
+      this.showPreview = this.hasPreview;
+      if (this.showPreview) {
+        this.getImagePreviews();
+      }
       // If autoSubmit is active we submit the files automatically
       if (this.autoSubmit) {
         this.submitFiles();
@@ -316,9 +254,11 @@ export default {
           // to update the src on the file preview.
           reader.addEventListener(
             "load",
-            function() {
-              this.$refs[`image${parseInt(i)}`][0].src = reader.result;
-            }.bind(this),
+            () => {
+              this.$nextTick(function() {
+                this.$refs[`${this.id}-image-${i}`][0].src = reader.result;
+              });
+            },
             false
           );
 
@@ -328,10 +268,161 @@ export default {
           reader.readAsDataURL(this.files[i]);
         }
       }
+    },
+    /**
+     * Creates the file input section
+     */
+    createInput(architect) {
+      let root = architect.createLabel("file-label file-label-container");
+
+      // Creating the html input element
+      let input = architect.createInput("file-input");
+      input.setId(this.id);
+      input.setRef("files");
+      let inputAttrs = {
+        placeholder: this.placeholder,
+        disabled: this.disabled || this.isUploading,
+        validationScope: this.validationScope,
+        type: "file",
+        readonly: this.readonly,
+        accept: this.accept
+      };
+      input.setAttrs(inputAttrs);
+      input.addEvent("change", this.handleFilesUpload);
+
+      let fileCta = architect.createSpan("file-cta");
+      let fileIcon = architect.createSpan("file-icon");
+      let icon = architect.createIcon().setProps({
+        icon: this.$thisvui.icons.upload,
+        preserveDefaults: !this.overrideDefaults
+      });
+      let fileLabel = architect.createSpan("file-label");
+      let label = architect.createP();
+      label.innerHtml(
+        this.isInitialUpload
+          ? this.label
+          : `Uploading ${this.fileCount} files...`
+      );
+      let filename = architect.createSpan("file-name");
+      filename.innerHtml(this.fileName);
+
+      fileIcon.addChild(icon);
+      fileLabel.addChild(label);
+      fileCta.addChild(fileIcon);
+      fileCta.addChild(fileLabel);
+      root.addChild(input);
+      root.addChild(fileCta);
+      root.addChild(filename, this.fileHasName);
+      architect.addChild(root);
+    },
+    /**
+     * Creates the file buttons section
+     */
+    createButtons(architect) {
+      let root = architect.createElement(
+        TButtons,
+        this.getButtonContainerClasses
+      );
+      root.setProps({ hasAddons: true });
+
+      // Creating the reset button
+      let resetBtn = architect.createButton();
+      resetBtn.setProps({
+        icon: this.clearIcon,
+        targetClass: this.getClearClasses
+      });
+      resetBtn.addChild(this.clearText, !this.hideButtonsLabels, true);
+      resetBtn.addClick(this.reset);
+
+      // Creating the submit button
+      let submitBtn = architect.createButton();
+      submitBtn.setProps({
+        icon: this.submitIcon,
+        targetClass: this.getSubmitClasses
+      });
+      submitBtn.addChild(this.submitText, !this.hideButtonsLabels, true);
+      submitBtn.addClick(this.submitFiles);
+
+      root.addChild(resetBtn, !this.isUploading);
+      root.addChild(submitBtn, this.showSubmitButton);
+
+      architect.addChild(root, this.hasFiles);
+    },
+
+    /**
+     * Creates the file preview section
+     */
+    createPreview(architect) {
+      if (this.showPreview && this.hasPreview) {
+        let root = architect.createDiv(this.getPreviewClasses);
+
+        let h2 = architect.createH("is-size-6", 2);
+        h2.innerHtml(`Uploaded ${this.files.length} file(s) successfully.`);
+
+        // Creating the reset button
+        let thumbnails = architect.createElement(TThumbnails);
+        for (let index in this.files) {
+          let file = this.files[index];
+          let thumbnail = architect.createElement(TThumbnail);
+          thumbnail.setKey(`${this.id}-thumb-${index}`);
+          let img = architect.createImg("image is-128x128");
+          img.setKey(`${this.id}-image-${index}`);
+          img.setRef(`${this.id}-image-${index}`, true);
+
+          let name = architect.createH();
+          name.innerHtml(file.name);
+
+          thumbnail.addChild(img);
+          thumbnail.addChild(name);
+          thumbnails.addChild(thumbnail);
+        }
+        root.addChild(h2, this.isUploadSuccess);
+        root.addChild(thumbnails);
+
+        architect.addChild(root);
+      }
+    },
+    /**
+     * Creates the fail box section
+     */
+    createFailBox(architect) {
+      if (this.isUploadFailed) {
+        let root = architect.createElement(TBox);
+
+        let h2 = architect.createH("is-size-6", 2);
+        h2.innerHtml(`Upload failed.`);
+
+        // Creating the reset button
+        let resetBtn = architect.createButton(this.getClearClasses);
+        resetBtn.setProps({ icon: this.clearIcon });
+        resetBtn.innerHtml(this.clearText, !this.hideButtonsLabels);
+        resetBtn.addClick(this.reset);
+
+        let error = architect.createElement("pre");
+        error.innerHtml(this.uploadError);
+
+        root.addChild(h2);
+        root.addChild(resetBtn);
+        root.addChild(error);
+
+        architect.addChild(root);
+      }
     }
+  },
+  render: function(h) {
+    let root = new ElementArchitect(h, "div", this.getContainerClasses);
+
+    let content = root.createDiv(this.getClasses);
+
+    this.createInput(content);
+    this.createButtons(content);
+    root.addChild(content);
+
+    this.createPreview(root);
+    this.createFailBox(root);
+    return root.create();
   },
   mounted() {
     this.reset();
   }
 };
-</script>
