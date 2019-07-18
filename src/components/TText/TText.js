7f2657f27c466393fdd88abc86fdece120b48cbd
ncanatta <ncanatta@gmail.com>
2019-07-18 15:47:35 -0400

    Implements render function in TText component
    1) A render function was implemented to render the TText component
    more efficiently and reduce bundle size.
    
    2) Added flex direction support to text container.

--- src/components/TText/TText.js
+++ src/components/TText/TText.js
@@ -1,51 +1,19 @@
-<template>
-  <div :id="id" :class="getContainerClass">
-    <div v-if="label" class="has-text-weight-normal">
-      <span
-        v-if="labelIcon !== undefined && !labelIconRight"
-        :class="getLabelIconClass"
-      >
-        <t-icon :icon="labelIcon"></t-icon>
-      </span>
-      <span :class="getLabelClass">{{ label }}</span>
-      <span
-        v-if="labelIcon !== undefined && labelIconRight"
-        :class="getLabelIconClass"
-      >
-        <t-icon :icon="labelIcon"></t-icon>
-      </span>
-    </div>
-    <div v-if="value">
-      <span
-        v-if="icon !== undefined && !labelIconRight"
-        :class="getLabelIconClass"
-      >
-        <t-icon :icon="icon"></t-icon>
-      </span>
-      <span :class="getValueClass">{{ value }}</span>
-      <span
-        v-if="icon !== undefined && labelIconRight"
-        :class="getLabelIconClass"
-      >
-        <t-icon :icon="icon"></t-icon>
-      </span>
-    </div>
-  </div>
-</template>
-
-<script>
 import utils from "../../utils/utils";
-import CssArchitect from "../../utils/css-architect";
+
 import helpers from "../../mixins/helpers";
 import common from "../../mixins/common";
 import icons from "../../mixins/icons";
 import TIcon from "../TIcon/TIcon";
 import dimension from "../../mixins/dimension";
+import flex from "../../mixins/flex";
+
+import CssArchitect from "../../utils/css-architect";
+import ElementArchitect from "../../utils/element-architect";
 
 export default {
   name: "t-text",
   components: { TIcon },
-  mixins: [common, helpers, icons, dimension],
+  mixins: [common, helpers, icons, dimension, flex],
   props: {
     name: {
       type: String
@@ -67,15 +35,19 @@ export default {
     icon: {
       type: String
     },
+    iconRight: {
+      type: Boolean,
+      default: false
+    },
+    iconClass: {
+      type: String
+    },
     labelIcon: {
       type: String
     },
     labelIconRight: {
       type: Boolean,
       default: false
-    },
-    iconClass: {
-      type: String
     },
     labelIconClass: {
       type: String
@@ -104,7 +76,7 @@ export default {
      * Dynamically build the css classes for the container element
      * @returns { A String with the chained css classes }
      */
-    getContainerClass: function() {
+    getContainerClasses: function() {
       const cssArchitect = new CssArchitect("t-text");
       cssArchitect.isFlexible("row", "start", "normal", false, "normal");
       cssArchitect.addClass(
@@ -115,6 +87,7 @@ export default {
       cssArchitect.addClass("has-text-weight-bold", this.bold);
       cssArchitect.addClass(this.getHelpersModifiers);
       cssArchitect.addClass(this.getDimensionModifiers);
+      cssArchitect.addClass(this.getFlexModifiers);
       return cssArchitect.getClasses();
     },
     /**
@@ -141,7 +114,9 @@ export default {
      * @returns { A String with the chained css classes }
      */
     getIconClass: function() {
-      const cssArchitect = new CssArchitect("icon is-small is-left");
+      const cssArchitect = new CssArchitect(
+        "icon is-small is-left is-inline-flex"
+      );
       cssArchitect.addClass(this.iconClass, this.iconClass !== undefined);
       return cssArchitect.getClasses();
     },
@@ -181,6 +156,89 @@ export default {
       }
       return transformClass;
     }
+  },
+  methods: {
+    /**
+     * Creates the icon
+     */
+    createIcons(architect, createStateIcon = true) {
+      // Creating the icon for the input
+      if (this.icon) {
+        let inputIcon = architect.createIcon(this.getIconClass);
+        inputIcon.addProp("icon", this.icon);
+        architect.addChild(inputIcon, this.icon);
+      }
+
+      // Creating the icon to display when validation passed
+      if (this.showValidStateIcon && !this.hideStateIcon && createStateIcon) {
+        let inputIconRight = architect.createIcon(this.getValidStateIconClass);
+        inputIconRight.addProp("icon", this.$thisvui.icons.check);
+        inputIconRight.addProp("preserveDefaults", !this.overrideDefaults);
+        architect.addChild(inputIconRight);
+      }
+    },
+    /**
+     * Creates the label icon
+     */
+    createIcon(architect, classses, conditionStatement) {
+      if (conditionStatement) {
+        let labelIconContainer = architect.createIcon(this.getLabelIconClass);
+        let labelIcon = architect.createIcon();
+        labelIcon.addProp("icon", this.labelIcon);
+        labelIconContainer.addChild(labelIcon);
+        architect.addChild(labelIconContainer, labelIcon);
+      }
+    },
+    /**
+     * Creates the label
+     */
+    createLabel(architect) {
+      if (this.label) {
+        let labelContainer = architect.createDiv("has-text-weight-normal");
+        this.createIcon(
+          labelContainer,
+          this.getLabelIconClass,
+          this.labelIcon && !this.labelIconRight
+        );
+        let label = architect.createSpan(this.getLabelClass);
+        label.innerHTML(this.label);
+        labelContainer.addChild(label);
+        this.createIcon(
+          labelContainer,
+          this.getLabelIconClass,
+          this.labelIcon && this.labelIconRight
+        );
+        architect.addChild(labelContainer);
+      }
+    },
+    /**
+     * Creates the Value
+     */
+    createValue(architect) {
+      if (this.value) {
+        let valueContainer = architect.createDiv("has-text-weight-normal");
+        this.createIcon(
+          valueContainer,
+          this.getIconClass,
+          this.icon && !this.iconRight
+        );
+        let value = architect.createSpan(this.getValueClass);
+        value.innerHTML(this.value);
+        valueContainer.addChild(value);
+        this.createIcon(
+          valueContainer,
+          this.getIconClass,
+          this.icon && this.iconRight
+        );
+        architect.addChild(valueContainer);
+      }
+    }
+  },
+  render: function(h) {
+    let root = new ElementArchitect(h, "div", this.getContainerClasses);
+    root.setId(this.id);
+    this.createLabel(root);
+    this.createValue(root);
+    return root.create();
   }
 };
-</script>
