e7756731218f3c28ac64b236369f8a6120319f97
ncanatta <ncanatta@gmail.com>
2019-11-09 13:43:23 -0300

    Refactor Image component
    TImage component was redesigned from scratch with new features

--- src/components/TImage/TImage.js
+++ src/components/TImage/TImage.js
@@ -1,26 +1,35 @@
 import helper from "../../mixins/helpers";
 import common from "../../mixins/common";
+import dimension from "../../mixins/dimension";
+import aspect from "../../mixins/aspect";
+
 import CssArchitect from "../../utils/css-architect";
-import ElementArchitect from "../../utils/element-architect";
+import { createDiv } from "../../utils/element-architect";
+
 
 export default {
   name: "t-image",
-  mixins: [common, helper],
+  mixins: [common, dimension, aspect, helper],
   props: {
     src: {
       type: String,
       required: true
     },
-    size: {
-      type: String
-    },
     alt: {
       type: String,
       default: "Image"
     },
-    isRounded: {
-      type: Boolean,
-      default: false
+    text: {
+      type: String
+    },
+    textClass: {
+      type: String
+    },
+    fillClass: {
+      type: String
+    },
+    circular: {
+      type: Boolean
     },
     targetClass: {
       type: String
@@ -28,36 +37,69 @@ export default {
   },
   computed: {
     /**
-     * Dynamically build the css classes for the figure element
+     * Dynamically build the css classes for the wrapper element
      * @returns { A String with the chained css classes }
      */
-    getFigureClass: function() {
-      const cssArchitect = new CssArchitect("image");
-      cssArchitect.addClass(this.getHelpersModifiers);
-      cssArchitect.addClass(this.size, this.size !== undefined);
-      return cssArchitect.getClasses();
+    getWrapperCss: function() {
+      const css = new CssArchitect("image");
+      css.addClass("circular", this.circular);
+      css.addClass(this.getHelpersModifiers);
+      css.addClass(this.getAspectRatioModifiers);
+      css.addClass(this.getDimensionModifiers);
+      css.addStyles([this.getDimensionStyles]);
+      return css;
     },
     /**
      * Dynamically build the css classes for the img element
      * @returns { A String with the chained css classes }
      */
-    getImgClass: function() {
-      const cssArchitect = new CssArchitect();
-      cssArchitect.addClass("is-rounded", this.isRounded);
-      cssArchitect.addClass(this.targetClass);
-      return cssArchitect.getClasses();
+    getImgCss: function() {
+      const css = new CssArchitect();
+      css.addClass(this.targetClass);
+      return css;
+    },
+    /**
+     * Dynamically build the css classes for the fill element
+     * @returns { A String with the chained css classes }
+     */
+    getFillCss: function() {
+      const css = new CssArchitect("image__fill");
+      css.addClass(this.fillClass);
+      return css;
+    },
+    /**
+     * Dynamically build the css classes for the text element
+     * @returns { A String with the chained css classes }
+     */
+    getTextCss: function() {
+      const css = new CssArchitect("image__text");
+      css.addClass(this.textClass);
+      return css;
     }
   },
   render: function(h) {
-    let root = new ElementArchitect(h, "figure", this.getFigureClass);
+    let root = createDiv(h, this.getWrapperCss.getClasses());
     root.setId(this.id);
+    root.setStyles(this.getWrapperCss.getStyles());
 
     // Creating the img element
-    let img = root.createImg(this.getImgClass);
+    let img = root.createImg(this.getImgCss.getClasses());
     img.addAttr("src", this.src);
     img.addAttr("alt", this.alt);
-
     root.addChild(img);
+
+    // Creating the fill element if present
+    if(this.isNotNull(this.fillClass)){
+      let fill = root.createDiv(this.getFillCss.getClasses());
+      root.addChild(fill);
+    }
+
+    // Creating the text element if present
+    if(this.isNotNull(this.text)){
+      let text = root.createDiv(this.getTextCss.getClasses());
+      text.innerHTML(this.text);
+      root.addChild(text);
+    }
     return root.create();
   }
 };
