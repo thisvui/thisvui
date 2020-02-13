d844a73475b86ceb84620b2bd438c8e337538bf1
ncanatta <ncanatta@gmail.com>
2020-02-13 15:05:03 -0300

    Improves overlay-box directive
    Adds minor fixes and improvements to overlay-box directive.

--- src/directives/overlay-box.js
+++ src/directives/overlay-box.js
@@ -1,7 +1,21 @@
 function calculatePos($box, $containerDimension, scrollTop, binding) {
+  let { width } = getAttributes($box);
   let startPos = $containerDimension.left + "px";
+
   let topPos =
     scrollTop + $containerDimension.top + $containerDimension.height + "px";
+
+  if (width) {
+    let leftOffset = 0;
+    if ($containerDimension.width > width) {
+      leftOffset = ($containerDimension.width - width) / 2;
+      startPos = $containerDimension.left + leftOffset  + "px";
+    }
+    if (width > $containerDimension.width) {
+      leftOffset = (width - $containerDimension.width) / 2;
+      startPos = $containerDimension.left - leftOffset  + "px";
+    }
+  }
 
   let yPos = topPos;
   let xPos = startPos;
@@ -59,13 +73,15 @@ function addCallbacks(el, binding) {
 function getAttributes(el) {
   let showOn = el.getAttribute("show-box") == "true";
   let target = el.getAttribute("box-target");
-  return { showOn, target };
+  let width = el.getAttribute("box-width");
+  return { showOn, target, width };
 }
 
 function setAttributes(el, args) {
-  let { showOn, target } = args;
+  let { showOn, target, width } = args;
   el.setAttribute("show-box", showOn);
   el.setAttribute("box-target", target);
+  el.setAttribute("box-width", width);
 }
 
 function compareBinding(binding) {
@@ -75,18 +91,18 @@ function compareBinding(binding) {
 export default {
   name: "overlay-box",
   bind: function(el, binding) {
-    let { target, showOn = true } = binding.value;
-    setAttributes(el, { showOn, target });
+    let { target, showOn = true, width } = binding.value;
+    setAttributes(el, { showOn, target, width });
 
     let $box = el;
     toggleDisplay($box, "none");
     addCallbacks(el, binding);
   },
   componentUpdated: function(el, binding) {
-    let { showOn = true, target } = binding.value;
+    let { showOn = true, target, width } = binding.value;
     let skip = compareBinding(binding);
     if (!skip) {
-      setAttributes(el, { showOn, target });
+      setAttributes(el, { showOn, target, width });
       if (showOn) {
         if (el.parentNode && "BODY" != el.parentNode.tagName) {
           el.parentNode.removeChild(el);
