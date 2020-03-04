7f21579018854990c2d64f2a4e5db2350856480b
ncanatta <ncanatta@gmail.com>
2020-03-04 18:27:04 -0300

    Improve click-outside directive
    - Makes minor refactor on click-outside directive code to
      improve maintainability.
    - Fixes minor bug on click-outside directive.

--- src/directives/click-outside.js
+++ src/directives/click-outside.js
@@ -1,42 +1,65 @@
-function handleOutsideClick(event, el, binding, vnode) {
-  event.stopPropagation();
-  const { handler, exclude } = binding.value;
+/**
+ * Check if this excluded element is the same or parent of the element the user just clicked on
+ */
+function checkIfParent(event, el) {
+  let parent = event.target.parentElement;
+  let exist = parent && el && el.id && parent.id;
+
+  if (exist) {
+    return el.id === parent.id;
+  }
+  return false;
+}
+
+/**
+ * Check if target element is contained in source element
+ */
+function elContained(source, target) {
+  if (source) {
+    return source.contains(target);
+  }
+  return false;
+}
+
+function clickedOnExcludedEl(event, vnode, exclude) {
   let clickedOnExcludedEl = false;
   if (exclude) {
     exclude.forEach(refName => {
       // We only run this code if there are exclude elements
       if (!clickedOnExcludedEl) {
         // Get the element using the reference name
-        let excludedEl = vnode.context.$refs[refName];
-        if(excludedEl){
-          excludedEl = excludedEl.$el || excludedEl
+        let $excludedEl = vnode.context.$refs[refName];
+        if ($excludedEl) {
+          $excludedEl = $excludedEl.$el || $excludedEl;
         }
-        let parent = event.target.parentElement;
-        let isParent = parent && excludedEl && excludedEl.id === parent.id;
-        let containsEl = excludedEl && excludedEl.contains(event.target);
-        // Check if this excluded element
-        // is the same element the user just clicked on
-        clickedOnExcludedEl = isParent || containsEl;
+
+        clickedOnExcludedEl =
+          checkIfParent(event, $excludedEl) ||
+          elContained($excludedEl, event.target);
       }
     });
   }
+  return clickedOnExcludedEl;
+}
 
-  let targetEl = event.target;
-  let targetElParent = event.target.parentElement;
-  let containsElement = el.contains(targetEl);
+function handleOutsideClick(event, el, binding, vnode) {
+  event.stopPropagation();
+  const { handler, exclude } = binding.value;
+
+  let $targetElParent = event.target.parentElement;
   let containsParentId =
-    targetElParent.id && el.querySelector(`#${targetElParent.id}`) != null;
-  let containsParent =
-    targetElParent && (el.contains(targetElParent) || containsParentId);
-  let contains = containsElement || containsParent;
-  if (!contains && !clickedOnExcludedEl) {
+    $targetElParent.id && el.querySelector(`#${$targetElParent.id}`) != null;
+  let contains =
+    elContained(el, event.target) ||
+    elContained(el, $targetElParent) ||
+    containsParentId;
+  if (!contains && !clickedOnExcludedEl(event, vnode, exclude)) {
     // Calls the handler to executed when clicked outside
-    if(vnode.context[handler]){
+    if (vnode.context[handler]) {
       vnode.context[handler](event);
-    }else{
+    } else {
       handler(event);
     }
-
   }
 }
 
